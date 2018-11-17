9cffd982d37b5dd113197a2c971a6ea8f15a0ec4
nvisary <nvisary@gmail.com>
2018-11-17 17:45:26 +0300

    Update

--- src/com/nvisary/RunClient.java
+++ src/com/nvisary/RunClient.java
@@ -3,11 +3,17 @@ package com.nvisary;
 import com.nvisary.tcp.TcpClient;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 public class RunClient {
     public static void main(String[] args) throws IOException {
-
-        TcpClient client = new TcpClient("localhost", 7777);
+        System.out.println("Please input server ip and port.");
+        System.out.print("Server ip: ");
+        Scanner scanner = new Scanner(System.in);
+        String ip = scanner.nextLine();
+        System.out.println("Server port: ");
+        int port = scanner.nextInt();
+        TcpClient client = new TcpClient(ip, port);
         client.runClient();
     }
 }
--- src/com/nvisary/RunServer.java
+++ src/com/nvisary/RunServer.java
@@ -6,11 +6,15 @@ import com.nvisary.tcp.TcpServer;
 import java.io.IOException;
 import java.net.NetworkInterface;
 import java.net.SocketException;
+import java.util.Scanner;
 
 public class RunServer {
 
     public static void main(String[] args) throws IOException {
-        TcpServer server = new TcpServer(7777);
+        System.out.print("Please input server running port: ");
+        Scanner scanner = new Scanner(System.in);
+        int port = scanner.nextInt();
+        TcpServer server = new TcpServer(port);
         server.startServer();
     }
 }
