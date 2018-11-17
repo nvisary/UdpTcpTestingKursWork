da105244105c38071bf8ebd1e990ec38917a6f6a
nvisary <nvisary@gmail.com>
2018-11-17 19:16:54 +0300

    update

--- src/com/nvisary/tcp/TcpClient.java
+++ src/com/nvisary/tcp/TcpClient.java
@@ -1,9 +1,8 @@
 package com.nvisary.tcp;
 
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import java.io.*;
 import java.net.Socket;
+import java.nio.file.Files;
 import java.util.Scanner;
 
 public class TcpClient {
@@ -17,66 +16,45 @@ public class TcpClient {
         this.serverPort = serverPort;
     }
 
+    public byte[] loadByteArrayFromFile(String fileName) throws IOException {
+        File file = new File(fileName);
+        byte[] result = Files.readAllBytes(file.toPath());
+        return result;
+    }
+
+    public void saveByteArrayToFile(byte[] arr, String fileName) throws IOException {
+        FileOutputStream fos = new FileOutputStream(fileName);
+        fos.write(arr);
+        fos.close();
+    }
+
     public void runClient() throws IOException, InterruptedException, ClassNotFoundException {
         Socket socket = new Socket(serverIp, serverPort);
         objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
         objectInputStream = new ObjectInputStream(socket.getInputStream());
-
-        while (true) {
+        int count = 0;
+        String filename = "img/image.jpg";
+        System.out.println("Load file..");
+        byte[] fileToSend = loadByteArrayFromFile(filename);
+        while (count < 10) {
             float sendingTime;
             float receivingTime;
+
             System.out.println("Try to send...");
             sendingTime = System.nanoTime();
-            objectOutputStream.writeObject(new byte[10000000]);
+            objectOutputStream.writeObject(fileToSend);
             System.out.println("Send.");
             receivingTime = System.nanoTime();
+
             System.out.println("Try to receiving...");
-            objectInputStream.readObject();
+            byte[] tmp = (byte[]) objectInputStream.readObject();
             System.out.println("Receive.");
             System.out.println("Packet time: " + (receivingTime - sendingTime) * 1000 + " seconds");
-            System.out.println("Thread sleep...");
-            Thread.sleep(3000);
+            System.out.println("File size: " + fileToSend.length / 1000 + " Kbytes");
+            String newFileName = "tmp" + count + "jpg";
+            System.out.println("Saving file with name: " + newFileName);
+            saveByteArrayToFile(tmp, newFileName);
+            count++;
         }
-
-        /*Thread receiveMessageThread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    boolean isMessagingRunning = true;
-                    while (isMessagingRunning) {
-                        String message = (String) objectInputStream.readObject();
-                        System.out.println("Server: " + message);
-                        isMessagingRunning = !message.equals("/stop");
-                    }
-
-
-                } catch (IOException | ClassNotFoundException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-
-        Thread sendMessageThread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    boolean isMessaging = true;
-                    while (isMessaging) {
-                        Scanner scanner = new Scanner(System.in);
-                        String message = scanner.nextLine();
-                        sendingTime = System.nanoTime();
-                        objectOutputStream.writeObject(message);
-                        isMessaging = !message.equals("/stop");
-                    }
-
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-
-
-        sendMessageThread.start();
-        receiveMessageThread.start();*/
     }
 }
